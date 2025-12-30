91b0c4aed883e51861edc06f496cd471ebd30e49
ArMonarch <praffulthapa12@gmail.com>
2025-12-30 21:41:38 +0545

    feat: added lunar host

---
+++ hosts/lunar/fs.nix
@@ -0,0 +1,29 @@
+{username, ...}: {
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/211daeba-5b1c-4a2e-8fcb-137aced84fac";
+      fsType = "btrfs";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/0F70-D526";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
+
+    # Windows partation mount as /home/${username}/Windows.
+    "/home/${username}/Windows" = {
+      device = "/dev/disk/by-uuid/62A443AFA443850F";
+      fsType = "ntfs-3g";
+      options = [
+        "rw"
+        "uid=1000"
+        "gid=1000"
+        "x-gvfs-name=Windows"
+      ];
+    };
+  };
+}
