5543cb1f3148655b7b44f35fea12dba5f174415b
ArMonarch <praffulthapa12@gmail.com>
2026-01-03 22:49:53 +0545

    feat: greetd support for niri and hyprland

--- modules/aspects/displayManager/greetd/default.nix
+++ modules/aspects/displayManager/greetd/default.nix
@@ -1,25 +1,31 @@
 {
-  niri ? true,
-  plasma ? false,
-  hyperland ? false,
+  niri ? false,
+  hyprland ? false,
 }: {
   username,
   pkgs,
   ...
-}: {
+}: let
+  compositor_startcmd =
+    if niri == true
+    then "niri-session"
+    else if hyprland == true
+    then "hyprland"
+    else "";
+in {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         user = username;
-        command = "${pkgs.lib.getExe pkgs.tuigreet} --cmd=niri";
+        command = "${pkgs.lib.getExe pkgs.tuigreet} --cmd=${compositor_startcmd}";
       };
     };
   };
 
   assertions = [
     {
-      assertion = niri == true || plasma == true || hyperland == true;
+      assertion = niri == true || hyprland == true;
       message = "any one {`hyperland` `niri` `plasma`} wayland compositer must be enabled";
     }
   ];
