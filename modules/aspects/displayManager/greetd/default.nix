e7283f2381eba5ed2632ca5e81fb1452956e4b19
ArMonarch <praffulthapa12@gmail.com>
2026-01-04 09:57:55 +0545

    feat; updated tuigreeter for greetd display manager

--- modules/aspects/displayManager/greetd/default.nix
+++ modules/aspects/displayManager/greetd/default.nix
@@ -1,26 +1,46 @@
 {
   niri ? false,
   hyprland ? false,
-}: {
-  username,
-  pkgs,
-  ...
-}: let
+}: {pkgs, ...}: let
   compositor_startcmd =
     if niri == true
     then "niri-session"
     else if hyprland == true
     then "hyprland"
     else "";
+  theme = "--theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red;'";
+  time_format = "--time --time-format '%I:%M %p | %a • %h | %F'";
 in {
   services.greetd = {
     enable = true;
     settings = {
+      terminal = {
+        vt = 1;
+      };
       default_session = {
-        user = username;
-        command = "${pkgs.lib.getExe pkgs.tuigreet} --cmd=${compositor_startcmd}";
+        command = "${pkgs.tuigreet}/bin/tuigreet --remember --asterisks --container-padding 2 ${time_format} --cmd ${compositor_startcmd}";
+        user = "greeter";
       };
     };
+  };
+
+  environment.corePackages = with pkgs; [
+    tuigreet
+  ];
+
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   assertions = [
