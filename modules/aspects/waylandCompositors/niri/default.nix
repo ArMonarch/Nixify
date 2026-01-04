214492f76ab6348721c9991a6cf4496c29b51176
ArMonarch <praffulthapa12@gmail.com>
2026-01-04 21:14:15 +0545

    feat(niri): selected only necessary components for niri

--- modules/aspects/waylandCompositors/niri/default.nix
+++ modules/aspects/waylandCompositors/niri/default.nix
@@ -1,15 +1,22 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkForce;
+in {
   programs.niri = {
     enable = true;
+    useNautilus = false;
   };
 
   environment.corePackages = with pkgs; [
-    ghostty
-    fuzzel
-    xkeyboard-config
-    wl-clipboard
-    playerctl
     brightnessctl
+    fuzzel
+    playerctl
+    wl-clipboard
+    xkeyboard-config
+    xwayland-satellite
   ];
 
   fonts.packages = with pkgs; [
