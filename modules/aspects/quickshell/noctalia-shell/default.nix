9e1431620ee1ac5d4cedb178fceb3630e465ff78
ArMonarch <praffulthapa12@gmail.com>
2026-01-11 10:24:18 +0545

    redactor: aspect quickshell/noctalia-shell

--- modules/aspects/quickshell/noctalia-shell/default.nix
+++ modules/aspects/quickshell/noctalia-shell/default.nix
@@ -2,20 +2,19 @@
   self',
   pkgs,
   ...
-}: {
-  environment.corePackages = [
-    (self'.packages.noctalia-shell.override
-      {
-        inherit
-          (pkgs)
-          brightnessctl
-          cliphist
-          ddcutil
-          imagemagick
-          matugen
-          wget
-          wlsunset
-          ;
-      })
-  ];
+}: let
+  noctalia-shell = self'.packages.noctalia-shell.override {
+    inherit
+      (pkgs)
+      brightnessctl
+      cliphist
+      ddcutil
+      imagemagick
+      matugen
+      wget
+      wlsunset
+      ;
+  };
+in {
+  environment.corePackages = [noctalia-shell];
 }
