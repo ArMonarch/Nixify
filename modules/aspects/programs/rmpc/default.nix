32df24ea99f1e36e485ec3714685819ec255a207
ArMonarch <praffulthapa12@gmail.com>
2026-02-18 21:18:09 +0545

    feat: added aspect programs/ rmpc basic configuration

--- modules/aspects/programs/rmpc/default.nix
+++ modules/aspects/programs/rmpc/default.nix
@@ -1,3 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   environment.corePackages = with pkgs; [rmpc];
+
+  # rmpc config file
+  hjem.users.${username}.xdg.config.files = {
+    "rmpc/config.ron" = {
+      type = "copy";
+      source = ./config/default.ron;
+    };
+  };
 }
