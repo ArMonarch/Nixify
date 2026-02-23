6db5d0e3fddccb915770e4a37be554cdcb8f15aa
ArMonarch <praffulthapa12@gmail.com>
2026-02-23 22:30:41 +0545

    feat: added option for selecting configProfile and themeProfile with options

--- modules/aspects/programs/rmpc/default.nix
+++ modules/aspects/programs/rmpc/default.nix
@@ -1,15 +1,42 @@
 {
+  lib,
   pkgs,
+  config,
   username,
   ...
-}: {
-  environment.corePackages = with pkgs; [rmpc];
+}: let
+  cfg = config.nixify.aspect.programs.rmpc;
+in {
+  options = {
+    nixify.aspect.programs.rmpc = {
+      configProfile = lib.options.mkOption {
+        type = lib.types.enum ["default" "full" "minimal"];
+        default = "default";
+        description = "config profile name that rmpc uses";
+      };
 
-  # rmpc config file
-  hjem.users.${username}.xdg.config.files = {
-    "rmpc/config.ron" = {
-      type = "copy";
-      source = ./config/default.ron;
+      themeProfile = lib.options.mkOption {
+        type = lib.types.enum ["default" "full" "minimal"];
+        default = "default";
+        description = "theme profile name that rmpc uses";
+      };
+    };
+  };
+
+  config = {
+    environment.systemPackages = with pkgs; [rmpc];
+
+    hjem.users.${username}.xdg.config.files = {
+      # rmpc config file
+      "rmpc/config.ron" = {
+        type = "symlink";
+        source = ./config/${cfg.configProfile}.ron;
+      };
+      # rmpc theme file
+      "rmpc/themes/default.ron" = {
+        type = "symlink";
+        source = ./themes/${cfg.themeProfile}.ron;
+      };
     };
   };
 }
