2f17fd743b03aaf72b58891df4c3dc77bfd75b4e
ArMonarch <praffulthapa12@gmail.com>
2026-03-10 21:23:22 +0545

    fix: fixed screensharing issues on niri due to xdg-desktop-portal-gnome

--- modules/aspects/wayland/niri/default.nix
+++ modules/aspects/wayland/niri/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   username,
   ...
@@ -17,6 +18,30 @@
     xwayland-satellite
     nautilus
   ];
+
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "niri";
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = lib.mkForce (with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ]);
+    config = {
+      common = {
+        default = ["gtk"];
+      };
+      niri = {
+        default = "gnome";
+        "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
+        "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
+      };
+    };
+  };
 
   hjem.users.${username}.xdg.config.files = {
     "niri/config.kdl" = {
